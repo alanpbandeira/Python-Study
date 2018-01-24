621ae31a05fcd85c7f56a1dabcd012fa17264de5
Alan Bandeira <alan.p.bandeira@gmail.com>
2018-01-24 10:01:41 -0300

    log update

--- Web/Flask-Study/microblog/app/routes.py
+++ Web/Flask-Study/microblog/app/routes.py
@@ -93,7 +93,7 @@ def user(username):
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
 
     if form.validate_on_submit():
         current_user.username = form.username.data
@@ -104,6 +104,6 @@ def edit_profile():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
-    
+
     return render_template("edit_profile.html", title="Edit Profile", form=form)
 
